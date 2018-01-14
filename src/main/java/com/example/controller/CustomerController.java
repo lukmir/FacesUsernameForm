139937100f5b76ef64ae05c78ff4653d67ae83c4
lukmir <lukmir2@gmail.com>
2018-01-14 17:41:43 +0100

    Final version confirmed

---
+++ src/main/java/com/example/controller/CustomerController.java
@@ -0,0 +1,38 @@
+package com.example.controller;
+
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named(value = "customer")
+@RequestScoped
+public class CustomerController {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
